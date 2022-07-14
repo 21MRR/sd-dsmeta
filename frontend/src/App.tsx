cdc1a957dc768b37ac2509f17f4c7f4efe3374ef
Mauro Rezende <maurorezende2012@gmail.com>
2022-07-14 19:24:39 -0300

    Front End finalizado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,19 @@
-import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
+
 
 function App() {
-
-  return(
+  return (
     <>
-    <h1>Olá MRR !</h1>
-    <NotificationButton/>
-    <NotificationButton/>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container"></div>
+          <SalesCard/>
+        </section>
+      </main>
     </>
-  )
-  
+  );
 }
 
-export default App
+export default App;
